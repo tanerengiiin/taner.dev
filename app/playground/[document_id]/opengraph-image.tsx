1a083dc6d09d9f422bcbc39e4fdb5cb6dab438d5
tanerengiiin <tanerengin4444@gmail.com>
2024-07-24 20:13:34 +0300

    .

--- app/playground/[document_id]/opengraph-image.tsx
+++ app/playground/[document_id]/opengraph-image.tsx
@@ -14,18 +14,13 @@ export default async function Image({ params }: { params: { document_id: string 
     const doc = playgroundDocs.find((x) => x.id === params.document_id);
     return new ImageResponse(
         (
-            <div
-                style={{
-                    fontSize: 48,
-                    background: 'white',
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                {doc?.title ?? 'Taner'}
+            <div tw='bg-white w-full h-full p-4 flex flex-col justify-between items-start'>
+                <div>
+                    <div tw='inline-flex items-center gap-2'>
+                        <div tw='w-12 h-12 rounded-full bg-main' />
+                        <div tw='text-5xl text-neutral-800 font-medium'>/ Taner</div>
+                    </div>
+                </div>
             </div>
         ),
         {
