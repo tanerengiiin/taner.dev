95660a7ecf8b414fbe768b1962f5a31ca7e25b5a
tanerengiiin <tanerengin4444@gmail.com>
2024-07-25 10:41:03 +0300

    add og image

--- app/playground/[document_id]/opengraph-image.tsx
+++ app/playground/[document_id]/opengraph-image.tsx
@@ -14,7 +14,7 @@ export default async function Image({ params }: { params: { document_id: string 
     const doc = playgroundDocs.find((x) => x.id === params.document_id);
     return new ImageResponse(
         (
-            <div tw='bg-white w-full h-full p-4 flex flex-col justify-between items-start'>
+            <div tw='rounded-xl bg-white w-full h-full p-4 flex flex-col justify-between items-start'>
                 <div>
                     <div tw='inline-flex items-center gap-2'>
                         <div tw='w-12 h-12 rounded-full bg-main' />
